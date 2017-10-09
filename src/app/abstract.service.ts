e40a94c2bd43be17b3d9ac712b81cc44b8b66b6b
Alencar Machado <alencar.machado@ufsm.br>
2017-10-09 16:59:55 -0300

    atualicacao nas daos

--- src/app/abstract.service.ts
+++ src/app/abstract.service.ts
@@ -3,7 +3,7 @@ import { Http, Headers } from '@angular/http';
 export  class AbstractService{
 
     public readonly ip_producao = "http://200.132.36.170:8087/";
-    public readonly ip_teste = "http://localhost:8080/";
+    public readonly ip_teste = "http://localhost:8180/";
     public readonly BASE_URL = this.ip_teste+"appRotas-1.1";
 
     public headers : Headers;
