8a2687bf1f8bb8da8be69dfafe9c0cff058eae4f
Alencar Machado <alencarmachado@iMac-de-gmob2.local>
2017-10-16 20:54:31 -0200

    acertando commit

--- src/app/abstract.service.ts
+++ src/app/abstract.service.ts
@@ -4,7 +4,7 @@ export  class AbstractService{
 
     public readonly ip_producao = "http://200.132.36.170:8087/";
     public readonly ip_teste = "http://localhost:8080/";
-    public readonly BASE_URL = this.ip_teste+"appRotas-1.1";
+    public readonly BASE_URL = this.ip_producao+"appRotas-1.1";
 
     public headers : Headers;
     protected http : Http;
